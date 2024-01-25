fa920b9c5438cbcc3af1c5f1cb9e042108198368
shakori999 <shakori999@gmail.com>
2024-01-25 18:23:49 +0300

    make first cuntion for computer choise

--- java.js
+++ java.js
@@ -1,3 +1,10 @@
-console.log("Hello World");
+const items = ['Rock', 'Paper','Scissors']
 
+const random = Math.floor(Math.random() * items.length);
+
+function getComputerChoice(choice) {
+  return console.log(choice) 
+}
+
+getComputerChoice(items[random])
 
